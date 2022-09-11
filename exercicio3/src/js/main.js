ec2ba0baf281de07d4310399af1f68e523e96805
viniciusscastro <viniciuss.castro@hotmail.com>
2022-09-10 21:02:50 -0300

    inicialização de variavel

--- exercicio3/src/js/main.js
+++ exercicio3/src/js/main.js
@@ -1,9 +1,10 @@
 import masks from "./modules/masks.js";
+
 document.querySelectorAll('input').forEach($input => {
     const field = $input.dataset.js
 
     $input.addEventListener('input', e => {
-      e.target.value = masks[field](e.target.value)
+      e.target.value = new masks[field](e.target.value)
     }, false)
     
   })
